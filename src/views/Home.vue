81e927799be376b177014f56ffaba00ac06c1fa9
YYYeung <sheepyy039@gmail.com>
2023-04-23 14:12:07 +0800

    update path to video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <video-background src="src/assets/lion.mp4" style="height: 100vh">
+  <video-background src="@/assets/lion.mp4" style="height: 100vh">
     <v-container>
       <audio
         ref="audio"
